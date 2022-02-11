f337e91a137572e61aac9d06d2c4eb9f736c1ff6
Rene Saarsoo <nene@triin.net>
2022-02-11 19:20:29 +0200

    Simple movement

---
+++ Player.gd
@@ -0,0 +1,19 @@
+extends KinematicBody2D
+
+export var speed = 75
+
+func _physics_process(delta):
+    var direction = Vector2(0,0)
+    if Input.is_action_pressed("ui_left"):
+        direction.x = -1
+    if Input.is_action_pressed("ui_right"):
+        direction.x = +1
+    if Input.is_action_pressed("ui_up"):
+        direction.y = -1
+    if Input.is_action_pressed("ui_down"):
+        direction.y = +1
+    
+    if direction.x != 0 && direction.y != 0:
+        direction = direction.normalized()
+    
+    move_and_collide(speed * direction * delta)
