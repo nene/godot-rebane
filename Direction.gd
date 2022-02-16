d29e29905e6c3fe17f94480eca61667693f37d80
Rene Saarsoo <nene@triin.net>
2022-02-16 23:02:27 +0200

    Use the same AnimationTree in Frater

--- Direction.gd
+++ Direction.gd
@@ -2,19 +2,3 @@ extends Reference
 
 static func is_moving(direction: Vector2) -> bool:
     return direction != Vector2.ZERO
-
-static func to_facing(direction: Vector2) -> String:
-    if direction.x > 0:
-        return "right"
-    elif direction.x < 0:
-        return "left"
-    elif direction.y < 0:
-        return "up"
-    else:
-        return "down"
-
-static func animation_name(moving: bool, facing: String) -> String:
-    if moving:
-        return "w-" + facing
-    else:
-        return facing.substr(0, 1).to_upper()
--- Frater.gd
+++ Frater.gd
@@ -4,26 +4,29 @@ const Direction = preload("res://Direction.gd")
 
 export var speed = 50
 var direction = Vector2.ZERO
-var facing = "down"
 var rng = RandomNumberGenerator.new()
+onready var animationState = $AnimationTree.get("parameters/playback")
 
 func _ready():
     rng.randomize()
+    set_animation_direction(Vector2.DOWN)
 
 func change_direction():
     var nr = rng.randf()
     if nr < 0.05:
         direction = Vector2.ZERO
     elif nr < 0.1:
-        direction = Vector2.DOWN.rotated(rng.randf() * 2 * PI)
+        direction = Vector2.DOWN.rotated(rng.randf() * 2 * PI).normalized()
 
 func _physics_process(delta):
     if Direction.is_moving(direction):
-        facing = Direction.to_facing(direction)
-
-    $AnimationPlayer.play(Direction.animation_name(Direction.is_moving(direction), facing))
-
-    if direction.x != 0 && direction.y != 0:
-        direction = direction.normalized()
+        set_animation_direction(direction)
+        animationState.travel("Walk")
+    else:
+        animationState.travel("Idle")
     
     move_and_slide(speed * direction)
+
+func set_animation_direction(direction: Vector2):
+    $AnimationTree.set("parameters/Idle/blend_position", direction)
+    $AnimationTree.set("parameters/Walk/blend_position", direction)
