92cf6a318cd4204b7aa22b191ba1be6a9cb84856
Rene Saarsoo <nene@triin.net>
2022-02-16 22:54:43 +0200

    Switch Player animation to AnimationTree

--- Player.gd
+++ Player.gd
@@ -3,10 +3,13 @@ extends KinematicBody2D
 const Direction = preload("res://Direction.gd")
 
 export var speed = 75
-var facing = "down"
+onready var animationState = $AnimationTree.get("parameters/playback")
+
+func _ready():
+    set_animation_direction(Vector2.DOWN)
 
 func _physics_process(delta):
-    var direction = Vector2(0,0)
+    var direction = Vector2.ZERO
     if Input.is_action_pressed("ui_left"):
         direction.x = -1
     if Input.is_action_pressed("ui_right"):
@@ -15,13 +18,17 @@ func _physics_process(delta):
         direction.y = -1
     if Input.is_action_pressed("ui_down"):
         direction.y = +1
+
+    direction = direction.normalized()
     
     if Direction.is_moving(direction):
-        facing = Direction.to_facing(direction)
-
-    $AnimationPlayer.play(Direction.animation_name(Direction.is_moving(direction), facing))
-
-    if direction.x != 0 && direction.y != 0:
-        direction = direction.normalized()
+        set_animation_direction(direction)
+        animationState.travel("Walk")
+    else:
+        animationState.travel("Idle")
     
     move_and_slide(speed * direction)
+
+func set_animation_direction(direction: Vector2):
+    $AnimationTree.set("parameters/Idle/blend_position", direction)
+    $AnimationTree.set("parameters/Walk/blend_position", direction)
