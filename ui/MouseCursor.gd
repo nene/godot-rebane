3a9378e3ca0f44cec08c5e9c551b1dbaffc0233a
Rene Saarsoo <nene@triin.net>
2022-02-22 17:01:15 +0200

    Custom cursor

--- ui/MouseCursor.gd
+++ ui/MouseCursor.gd
@@ -1,7 +1,12 @@
 extends Node2D
 class_name MouseCursor
 
+var cursor_arrow = load("res://ui/cursor-arrow.png")
+
 var holding_item: GameItem setget set_holding_item
+
+func _ready():
+    Input.set_custom_mouse_cursor(cursor_arrow, Input.CURSOR_ARROW, Vector2.ZERO)
 
 func set_holding_item(item: GameItem):
     holding_item = item
