d43c3cbce0597563dd5bea712231d0ff5ab3519f
Rene Saarsoo <nene@triin.net>
2022-02-22 19:20:25 +0200

    Add counts to items

--- ui/Hud.gd
+++ ui/Hud.gd
@@ -1,14 +1,11 @@
 extends Control
 
 var popup_inventory = [
-    Book.new(),
-    BottleOpener.new(),
-    Drain.new(),
-    Tap.new(),
-    Ocean.new(),
-    Ocean.new(),
-    Ocean.new(),
-    Ocean.new(),
+    [Book.new(), 1],
+    [BottleOpener.new(), 1],
+    [Drain.new(), 1],
+    [Tap.new(), 1],
+    [Ocean.new(), 4],
 ]
 
 var hotbar_inventory = [
@@ -18,11 +15,22 @@ var hotbar_inventory = [
 ]
 
 func _ready():
+    popup_inventory = map(funcref(self, "set_count"), popup_inventory)
     $InventoryDialog.load_items(popup_inventory)
     $InventoryDialog.mouse_cursor = $MouseCursor
     $HotBar/InventoryGrid.load_items(hotbar_inventory)
     $HotBar/InventoryGrid.mouse_cursor = $MouseCursor
 
+func set_count(pair: Array):
+    pair[0].count = pair[1]
+    return pair[0]
+
 func _input(event):
     if event.is_action_pressed("ui_inventory"):
         $InventoryDialog.visible = !$InventoryDialog.visible
+
+func map(f: FuncRef, arr: Array) -> Array:
+    var result = []
+    for item in arr:
+        result.push_back(f.call_func(item))
+    return result
