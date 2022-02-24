9c7b9f3b53063ca62255fec5cb07f15d61ec2a13
Rene Saarsoo <nene@triin.net>
2022-02-24 14:13:34 +0200

    Configurable title and description of InventoryDialog

--- inventory/InventoryDialog.gd
+++ inventory/InventoryDialog.gd
@@ -1,4 +1,8 @@
+tool
 extends Control
+
+export var title = "Title" setget _set_title, _get_title
+export var description = "Description..." setget _set_description, _get_description
 
 # Dependency injection
 var mouse_cursor: MouseCursor setget set_mouse_cursor
@@ -8,3 +12,15 @@ func set_mouse_cursor(cursor: MouseCursor):
 
 func load_inventory(inventory: Inventory):
     $InventoryGrid.load_inventory(inventory)
+
+func _set_title(title: String):
+    $TitleText.text = title
+
+func _get_title() -> String:
+    return $TitleText.text
+
+func _set_description(description: String):
+    $DescriptionText.text = description
+
+func _get_description() -> String:
+    return $DescriptionText.text
