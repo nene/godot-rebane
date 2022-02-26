f6a6104563865ee87e76f44bdf4a54fced49afcb
Rene Saarsoo <nene@triin.net>
2022-02-26 17:17:37 +0200

    Only interact when player is closeby

--- furniture/InteractBox.gd
+++ furniture/InteractBox.gd
@@ -1,6 +1,7 @@
 extends Area2D
 
 signal interact
+var _player_is_close = false
 
 func _on_mouse_entered():
     InteractionState.enter_object()
@@ -11,4 +12,12 @@ func _on_mouse_exited():
 func _on_input(viewport, event, shape_idx):
     if event is InputEventMouseButton:
         if event.button_index == BUTTON_LEFT && event.pressed:
-            emit_signal("interact")
+            if _player_is_close:
+                emit_signal("interact")
+
+func _on_area_entered(area):
+    print("area entered")
+    _player_is_close = true
+
+func _on_area_exited(area):
+    _player_is_close = false
