2e232592b08f0045270a7e3f76e3e8aa2701a279
Rene Saarsoo <nene@triin.net>
2022-02-28 22:13:58 +0200

    Wait for next physics frame before changing location

--- furniture/DoorBox.gd
+++ furniture/DoorBox.gd
@@ -3,7 +3,10 @@ extends Area2D
 export(Location.Location) var to_location = Location.Location.CFE_CELLAR
 
 func _on_door_entered(body):
-    $Timer.start(0.00000001)
-
-func _on_timer():
+    # We use physics engine to move characters around,
+    # this method gets called in the middle of physics processing,
+    # so we can't start adding/removing nodes from the tree before
+    # physics has finished
+    yield(get_tree(), "physics_frame")
+    
     CurrentLocation.change(to_location)
