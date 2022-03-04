f45c0a7f97fbea66f6f823a2585042f317ebec96
Rene Saarsoo <nene@triin.net>
2022-03-04 11:02:27 +0200

    Fix multiline button auto-sizing

--- dialog/AnswerButton.gd
+++ dialog/AnswerButton.gd
@@ -1,17 +1,21 @@
 tool
 extends Button
 
-export var inner_text = "" setget _set_inner_text, _get_inner_text
+export var inner_text = "" setget _set_inner_text
 
 onready var _label = $MarginContainer/RichTextLabel
+const VERTICAL_MARGIN = 2
 
 func _set_inner_text(text):
     inner_text = text
     if _label:
         _label.text = text
-
-func _get_inner_text():
-    return inner_text
+        yield(get_tree(), "idle_frame")
+        rect_min_size.y = _label.get_content_height() + VERTICAL_MARGIN * 2
 
 func _ready():
-    _label.text = inner_text
+    _label.set_anchors_and_margins_preset(Control.PRESET_WIDE)
+    _set_inner_text(inner_text)
+
+func _on_visibility_changed():
+    _set_inner_text(inner_text)
