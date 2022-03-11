1b4e9abb520b8598436928265d2bd2f972b1f27e
Rene Saarsoo <nene@triin.net>
2022-03-11 17:21:57 +0200

    Use Light2D for CFE hall lights

--- lights/CfeLamp/CfeLamp.gd
+++ lights/CfeLamp/CfeLamp.gd
@@ -1,7 +1,12 @@
 extends Sprite
 
+func _ready():
+    $Light2D.hide()
+
 func toggle(active: bool):
     if active:
         self.frame = 1
+        $Light2D.show()
     else:
         self.frame = 0
+        $Light2D.hide()
