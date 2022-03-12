b636938d5e18bd46147f7d6c2e9264d578ee91ae
Rene Saarsoo <nene@triin.net>
2022-03-12 13:32:00 +0200

    Bottle movement using noise

--- minigames/opening/OpeningGame.gd
+++ minigames/opening/OpeningGame.gd
@@ -1,9 +1,31 @@
 extends Node2D
 
-onready var _opener = $Opener
-onready var _bottle_cap = $BottleCap
-var _ready_to_open = false
+const BOTTLE_START_POSITION = Vector2(100, 100)
+const BOTTLE_MAX_MOVEMENT = Vector2(150, 150)
 
+onready var _bottle = $Bottle
+onready var _bottle_cap = $BottleCap
+onready var _opener = $Opener
+var _ready_to_open = false
+var _noise = OpenSimplexNoise.new()
+var _time = 0
+
+func _ready():
+    _noise.seed = randi()
+    _noise.period = 2
+
+func _process(delta):
+    _time += delta
+    _set_bottle_position(BOTTLE_START_POSITION + _bottle_offset())
+
+func _bottle_offset() -> Vector2:
+    var x = _noise.get_noise_2d(_time, 1)
+    var y = _noise.get_noise_2d(1, _time)
+    return Vector2(x, y) * BOTTLE_MAX_MOVEMENT
+
+func _set_bottle_position(position: Vector2):
+    _bottle.position = position
+    _bottle_cap.position = position
 
 func _input(event):
     _opener.position = get_global_mouse_position()
