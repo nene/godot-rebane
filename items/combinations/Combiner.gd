8db41845ac35a1656c21861cff4bcb3d09b620e3
Rene Saarsoo <nene@triin.net>
2022-03-15 00:00:04 +0200

    Refactor complex conditional Combiner logic

--- items/combinations/Combiner.gd
+++ items/combinations/Combiner.gd
@@ -1,26 +1,45 @@
 extends Node
 
-func combine(item_in_hand: GameItem, item_in_slot: GameItem) -> Combination:
-    if item_in_hand.type() == "bottle_opener" && item_in_slot.type() == "beer_bottle"  && !item_in_slot.is_open():
-        var OpenBottle = load("res://items/combinations/OpenBottle.gd")
-        return OpenBottle.new(item_in_hand, item_in_slot)
-    if item_in_hand.type() == "beer_bottle" && !item_in_hand.is_open() && item_in_slot.type() == "bottle_opener":
-        var OpenBottle = load("res://items/combinations/OpenBottle.gd")
-        return OpenBottle.new(item_in_hand, item_in_slot)
-    elif item_in_hand.type() == "beer_glass" && item_in_hand.is_empty() && item_in_slot.type() == "beer_bottle" && item_in_slot.is_open() && !item_in_slot.is_empty():
-        var PourToGlass = load("res://items/combinations/PourToGlass.gd")
-        return PourToGlass.new(item_in_hand, item_in_slot)
-    elif item_in_hand.type() == "beer_bottle" && !item_in_hand.is_empty() && item_in_hand.is_open() && item_in_slot.type() == "beer_glass" && item_in_slot.is_empty():
-        var PourToGlass = load("res://items/combinations/PourToGlass.gd")
-        return PourToGlass.new(item_in_hand, item_in_slot)
-    elif (item_in_hand.type() == "beer_bottle" || item_in_hand.type() == "beer_glass") && !item_in_hand.is_empty() && item_in_slot.type() == "drain":
-        var EmptyToDrain = load("res://items/combinations/EmptyToDrain.gd")
-        return EmptyToDrain.new(item_in_hand, item_in_slot)
-    elif item_in_hand.type() == "ocean" && item_in_slot.type() == "drain":
-        var DiscardToDrain = load("res://items/combinations/DiscardToDrain.gd")
-        return DiscardToDrain.new(item_in_hand, item_in_slot)
-    elif (item_in_hand.type() == "beer_bottle" || item_in_hand.type() == "beer_glass") && item_in_hand.is_empty() && item_in_slot.type() == "tap":
-        var FillFromTap = load("res://items/combinations/FillFromTap.gd")
-        return FillFromTap.new(item_in_hand, item_in_slot)
-    else:
-        return null
+var possible_combinations = {
+    ["bottle_opener", "beer_bottle"]: "OpenBottle",
+    ["beer_glass", "beer_bottle"]: "PourToGlass",
+    ["drain", "beer_bottle"]: "EmptyToDrain",
+    ["drain", "beer_glass"]: "EmptyToDrain",
+    ["drain", "ocean"]: "DiscardToDrain",
+    ["tap", "beer_bottle"]: "FillFromTap",
+    ["tap", "beer_glass"]: "FillFromTap",
+}
+
+func combine(item_in_hand: GameItem, item_in_slot: GameItem):
+    var pair = [item_in_hand.type(), item_in_slot.type()]
+    if pair in possible_combinations:
+        var cls_name = possible_combinations[pair]
+        if call("can_" + cls_name, item_in_hand, item_in_slot):
+            return load_combination_class(cls_name).new(item_in_hand, item_in_slot)
+
+    var reverse_pair = [item_in_slot.type(), item_in_hand.type()]
+    if reverse_pair in possible_combinations:
+        var cls_name = possible_combinations[reverse_pair]
+        if call("can_" + cls_name, item_in_slot, item_in_hand):
+            return load_combination_class(cls_name).new(item_in_hand, item_in_slot)
+
+    return null
+
+func load_combination_class(cls_name: String):
+    return load("res://items/combinations/" + cls_name + ".gd")
+
+
+func can_OpenBottle(opener: BottleOpener, bottle: BeerBottle) -> bool:
+    return !bottle.is_open()
+
+func can_PourToGlass(glass: BeerGlass, bottle: BeerBottle) -> bool:
+    return bottle.is_open() && !bottle.is_empty() && glass.is_empty()
+
+func can_EmptyToDrain(drain: Drain, glass_or_bottle: GameItem) -> bool:
+    return !glass_or_bottle.is_empty()
+
+func can_DiscardToDrain(drain: Drain, ocean: Ocean) -> bool:
+    return true
+
+func can_FillFromTap(tap: Tap, glass_or_bottle: GameItem) -> bool:
+    return glass_or_bottle.is_empty()
