c93a29cd53b1333fc9bc81f8c61a37bcaa123d20
Rene Saarsoo <nene@triin.net>
2022-03-16 21:10:21 +0200

    Add beer-pouring sound

--- items/combinations/PourToGlass.gd
+++ items/combinations/PourToGlass.gd
@@ -6,4 +6,5 @@ func _init(in_hand, in_slot).(in_hand, in_slot):
 func _execute(glass: BeerGlass, bottle: BeerBottle):
     glass.fill(bottle.drink_type, BeerGlass.FULL)
     bottle.empty()
+    GameEvents.emit_signal("sound_play", "PouringBeer")
     emit_signal("success", self)
