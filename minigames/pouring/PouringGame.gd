6f61b978fb8db658589a36daf108abef94562f6b
Rene Saarsoo <nene@triin.net>
2022-03-18 21:51:45 +0200

    Use physics-based droplets in minigame

--- minigames/pouring/PouringGame.gd
+++ minigames/pouring/PouringGame.gd
@@ -1,11 +1,32 @@
 extends MiniGame
 
+const Droplet = preload("res://minigames/pouring/Droplet.tscn")
+
 onready var _bottle = $Bottle
-onready var _particles = $Particles
+onready var _droplets = $Droplets
+var _is_pouring = false
 
 func _ready():
     pass
 
 func _input(event):
     _bottle.position = get_global_mouse_position()
-    _particles.position = get_global_mouse_position()
+
+    if event is InputEventMouseButton && event.button_index == BUTTON_LEFT:
+        if event.pressed:
+            _is_pouring = true
+        else:
+            _is_pouring = false
+
+func _physics_process(delta):
+    if _is_pouring:
+        for i in 5:
+            _add_droplet()
+
+func _add_droplet():
+    var droplet = Droplet.instance()
+    droplet.position = get_global_mouse_position() + _random_offset()
+    _droplets.add_child(droplet)
+
+func _random_offset() -> Vector2:
+    return Vector2(randf(), randf())
