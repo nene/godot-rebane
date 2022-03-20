cbeaabb628ca0d0cbf9a71e9407a81150b085a4d
Rene Saarsoo <nene@triin.net>
2022-03-20 20:58:03 +0200

    Define flow_rate per each droplet

--- minigames/pouring/Droplet.gd
+++ minigames/pouring/Droplet.gd
@@ -1,17 +1,20 @@
 extends RigidBody2D
 
-signal hit_target
+signal hit_target(flow_rate)
 signal splash(position, diraction)
 
 const TableEdge = preload("res://minigames/pouring/TableEdge.gd")
 const GlassBottom = preload("res://minigames/pouring/GlassBottom.gd")
+
+# To be set on creation
+var flow_rate: float = 0.0
 
 func _on_timeout():
     queue_free()
 
 func _on_body_entered(body: Node):
     if body is GlassBottom:
-        emit_signal("hit_target")
+        emit_signal("hit_target", flow_rate)
     else:
         emit_signal("splash", self.position, _splash_direction(body as PhysicsBody2D))
     queue_free()
