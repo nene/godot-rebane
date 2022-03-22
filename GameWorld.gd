ea641d3090ceb92b352e3e96a5ff4354eede8cd3
Rene Saarsoo <nene@triin.net>
2022-03-22 22:25:14 +0200

    Only show currently active location

--- GameWorld.gd
+++ GameWorld.gd
@@ -7,7 +7,7 @@ onready var _locations = {
 }
 
 var _current_location_name: int
-onready var _current_location: Location
+var _current_location: Location
 
 func _ready():
     GameEvents.connect("change_location", self, "_change_location")
@@ -15,18 +15,27 @@ func _ready():
     _current_location_name = _find_location_with_player()
     _current_location = _locations[_current_location_name]
 
+    _hide_all_locations()
+    _current_location.show()
+
 func _find_location_with_player() -> int:
     for name in _locations:
         if _locations[name].has_player():
             return name
     return -1
 
+func _hide_all_locations():
+    for name in _locations:
+        _locations[name].hide()
+
 func _change_location(location_name: int):
     var old_location_name = _current_location_name
     var player = _current_location.remove_player()
+    _current_location.hide()
 
     _current_location_name = location_name
     _current_location = _locations[location_name]
+    _current_location.show()
 
     var spawn = _current_location.find_spawn_point(old_location_name)
 
